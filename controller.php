1db42b05e6f44fa0ab3868c2493c28ae0678bac3
ChristianUA <cdpeterson@email.arizona.edu>
2019-11-27 21:17:47 -0700

    Adding users and logging in

--- controller.php
+++ controller.php
@@ -1,27 +1,42 @@
 <?php
-include 'adaptor.php'
+include 'adaptor.php';
+session_start();
+$database = new DatabaseAdaptor();
 
 if(isset($_GET['mode'])) {
     if ($_GET['mode'] == "register") {
-        // code
+        // Add user record to register
+        $email = $_POST['email'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $password = $_POST['password'];
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Add address record and get id
+        $street_address = $_POST['street_address'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $zip = $_POST['zip'];
+        $address_id = $database->addAddress($street_address, $city, $state, $zip);
+
+        $database->registerUser($email, $first_name, $last_name, $address_id, $hashed_password);
+        // Redirect user to homepage
+        header("Location: index.html");
     }
     elseif ($_GET['mode'] == "login") {
-        // FROM 16_MultiPages lecture notes
-        if ($myDatabaseFunctions->verified($username, $password)) {
+        // check if user is valid
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        if ($database->verifyUser($email, $password)) {
             // Store session data so the account name isset and known on any page
-            $_SESSION ['user'] = $username;
-            // Return to the main page where the user's account name
-            // is known and $_SESSION ['user'] is set
-            header ( "Location: index.html" );
+            $_SESSION ['user'] = $email;
+            // Redirect user to homepage
+            header("Location: index.html");
         } else {
             $_SESSION ['loginError'] = 'Invalid Account/Password';
-            header ( "Location: ./login.php?mode=login" );
+            header("Location: ./login.php?mode=login");
         }
     }
-}
-
-if (isset ( $_POST ['login'] )) {
-
 }
 
 ?>
