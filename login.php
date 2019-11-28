0202d1bf757d0ea535fd484c2c99da896f8b5bbb
ChristianUA <cdpeterson@email.arizona.edu>
2019-11-27 21:18:21 -0700

    Created UI and database interaction

--- login.php
+++ login.php
@@ -6,46 +6,43 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
-        <link href="signin.css" rel="stylesheet">
     </head>
-    <body class="text-center" data-gr-c-s-loaded="true">
-        <form class="form-signin">
-      <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" data-keeper-lock-id="k-51fkn29j71">
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" data-keeper-lock-id="k-ahzgkwblz79">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">© 2017-2019</p>
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <form action="controller.php" method="post">
-            <div class="loginContainer">
-                <div class="labels">Username&nbsp;</div>
-                <div class="fields">
-                    <input type="text" id="username" name="username" required>
-                </div>
-                <div class="labels">Password&nbsp;</div>
-                <div class="fields">
-                    <input type="password" id="password" name="password" required>
-                </div>
-                <div class="fields">
-                    <input type="submit" name="register" value="Register"><br>
-
+    <body class="text-center">
+        <?php
+            session_start();
+        ?>
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                    <h2 class="mb-3">Login</h2>
+                    <form action="controller.php?mode=login" method="post">
+                      <div class="form-row">
+                          <div class="col-md-12 mb-3">
+                              <label for="email">Email address</label>
+                              <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+                          </div>
+                      </div>
+                      <div class="form-row">
+                          <div class="col-md-12 mb-4">
+                              <label for="password">Password</label>
+                              <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                          </div>
+                      </div>
+                      <?php
+                          if(isset($_SESSION ['loginError'])) {
+                              echo '<div class="alert alert-danger col-md-12" role="alert">' . $_SESSION['loginError'] . '</div>';
+                              unset($_SESSION ['loginError']);
+                          };
+                      ?>
+                      <button class="btn btn-primary" type="submit">Login</button>
+                    </form>
                 </div>
             </div>
-        </form>
+        </div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" crossorigin="anonymous"></script>
-    </div>
     </body>
 </html>
