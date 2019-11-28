9fca19b04d2b4342e62d380f6620604a18cde574
ChristianUA <cdpeterson@email.arizona.edu>
2019-11-27 21:17:13 -0700

    Added database interaction functions

--- adaptor.php
+++ adaptor.php
@@ -3,38 +3,48 @@ class DatabaseAdaptor {
     private $DB; // The instance variable used in every method
     // Connect to an existing data based named 'first'
     public function __construct() {
-        $dataBase =
-        'mysql:dbname=final_project;charset=utf8;host=127.0.0.1';
+        $dataBase = 'mysql:dbname=final_project;charset=utf8;host=127.0.0.1';
         $user = 'root';
         $password = ''; // Empty string with XAMPP install
         try {
-            $this->DB = new PDO ( $dataBase, $user, $password );
-            $this->DB->setAttribute ( PDO::ATTR_ERRMODE,
-                PDO::ERRMODE_EXCEPTION );
-        } catch ( PDOException $e ) {
+            $this->DB = new PDO($dataBase, $user, $password);
+            $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
             echo ('Error establishing Connection');
-            exit ();
+            exit();
         }
     }
 
-    public function getMoviesReleasedSince($year) {
-        $stmt = $this->DB->prepare( "SELECT * from movies where year >=" . $year);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function addAddress($street_address, $city, $state, $zip) {
+        // Create address and return the ID to be tied to user
+        $stmt = $this->DB->prepare("INSERT INTO addresses (street_address, city, state, zip) VALUES (?, ?, ?, ?);");
+        $stmt->execute([$street_address, $city, $state, $zip]);
+        return $this->DB->lastInsertId();
     }
 
-    public function registerUser($email, $password) {
-        // TODO: Write SQL statement
-        $stmt = $this->DB->prepare("SQL STATEMENT");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function registerUser($email, $first_name, $last_name, $address_id, $hashed_password) {
+        // Create user
+        $stmt = $this->DB->prepare("INSERT INTO users (email, first_name, last_name, address_id, password_hash) VALUES (?, ?, ?, ?, ?);");
+        $stmt->execute([$email, $first_name, $last_name, $address_id, $hashed_password]);
     }
 
     public function verifyUser($email, $password) {
-        // TODO: Write SQL statement
-        $stmt = $this->DB->prepare("SQL STATEMENT");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // Return true if user exists in database and password matches
+        $stmt = $this->DB->prepare("SELECT password_hash FROM users WHERE email=?");
+        $stmt->execute([$email]);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if(count($results) == 1) {
+            // select password_hash from first result
+            $password_hash = $results[0]['password_hash'];
+
+            if(password_verify($password, $password_hash)) {
+                return True;
+            }
+            return False;
+        }
+        return False;
     }
 }
 ?>
